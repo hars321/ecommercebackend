b38a11926a737d300f67a8a8d763f9ea419320ef
hars321 <54657843+hars321@users.noreply.github.com>
2020-10-30 16:35:31 +0530

    cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
   
   next();
 });
